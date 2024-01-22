d85b3191af4d8be2ef854a8023a8aca072391093
JerichElano <95858065+JerichElano@users.noreply.github.com>
2024-01-22 17:15:46 +0800

    Add files via upload
    fixed the problem about the infinity and update some formula

--- srtf(fullfinalist).py
+++ srtf(fullfinalist).py
@@ -28,51 +28,55 @@ def srtf_formula(jobs, arrival_time, burst_time):
         if next_job is None:
             current_time += 1
             continue
-
+        print("...........................................................")
         print(f'initialremaining {remaining_time[next_job]}\ntime {current_time}\nindel {indel}')
-            
         if len(sorted_AT) > indel:
-            if remaining_time[next_job] == sorted_AT[indel] - current_time:
+            #
+            if remaining_time[next_job] <= sorted_AT[indel] - current_time:
                 gantt_chart.append((jobs[next_job], remaining_time[next_job]))
                 #update remaining time of [next job]
                 f_jobs[next_job] = 1
-                current_time = sorted_AT[indel]
+                current_time = remaining_time[next_job] + current_time
                 turnaround_time[next_job] = current_time - arrival_time[next_job]
                 waiting_time[next_job] = turnaround_time[next_job] - burst_time[next_job]
-                print(f'three+++++++++++++++++remaining {arrival_time[next_job]} {remaining_time[next_job]}\ntime {current_time}\nindel {indel}')
+                print(f'three+++++++++++++++++\nremaining{remaining_time[next_job]}\ntime {current_time}\nindel {indel}')
                 print(turnaround_time, waiting_time, 00000000)
             else:
                 gantt_chart.append((jobs[next_job], sorted_AT[indel] - current_time))
                 #update remaining time of [next job]
                 remaining_time[next_job] = remaining_time[next_job] - (sorted_AT[indel] - current_time)
                 current_time = sorted_AT[indel]
-                print(f'two=============remaining {remaining_time[next_job]}\ntime {current_time}\nindel {indel}')
-            indel += 1
-        else:
+                indel += 1
+                print(f'two=============\nremaining{remaining_time[next_job]}\ntime {current_time}\nindel {indel}')
+
+        elif len(sorted_AT) == indel or current_time == sorted_AT.pop():
             print('fuck')
             gantt_chart.append((jobs[next_job], remaining_time[next_job]))
             current_time = current_time + remaining_time[next_job]
             turnaround_time[next_job] = current_time - arrival_time[next_job]
             waiting_time[next_job] = turnaround_time[next_job] - burst_time[next_job]
             f_jobs[next_job] = 1
-            print(f'one------------remaining {remaining_time[next_job]}\ntime {current_time}\nindel {indel}')
-            print(turnaround_time, waiting_time, 0000)
+            print(f'one------------\nremaining{remaining_time[next_job]}\ntime {current_time}\nindel {indel}')
+            print(turnaround_time, waiting_time, 0000)                    
+        
         print(gantt_chart)
         
     return arrival_time, burst_time, waiting_time, turnaround_time, gantt_chart
 
-def main():
-    '''
+def main(): 
+    
     jobs = ["A", "B", "C"]
     arrival_time = [0, 4, 7]
     burst_time = [7, 4, 1]
-
-    '''    
+    '''
+    jobs = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    arrival_time = [7, 4, 16, 0, 18, 12, 24, 4]
+    burst_time = [5, 6, 8, 12, 2, 7, 3, 8]
+    
     jobs = ["A", "B", "C", "D", "E"]
     arrival_time = [3, 5, 8, 0 , 12]
     burst_time = [4, 9, 4, 7, 6]
-    
-
+    '''
     print("======================================================")
     print("Welcome to the Shortest Remaining Time First Simulator")
     print("======================================================")
